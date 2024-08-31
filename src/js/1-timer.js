798151f72eef51fe66734f9015de2d82fce6fd92
Taras Chepil <agent000666@gmail.com>
2024-08-31 07:33:45 +0300

    r

---
+++ src/js/1-timer.js
@@ -0,0 +1,11 @@
+import flatpickr from 'flatpickr';
+import iziToast from 'izitoast';
+import 'flatpickr/dist/flatpickr.min.css';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const dateTimePick = document.getElementById('datetime-picker');
+const startBtn = document.querySelector('button[data-start]');
+const days = document.querySelector('span[data-days]');
+const hours = document.querySelector('span[data-hours]');
+const minutes = document.querySelector('span[data-minutes]');
+const seconds = document.querySelector('span[data-seconds]');
